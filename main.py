baea050f934e994349f823d14888c4f3688801c2
KWoznicki <krzwozn@gmail.com>
2019-08-28 20:37:21 +0200

    +Added possibility for user to choose arguments values in command line

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from PIL import Image
 import os
-import sys
 from datetime import datetime
 import random
+import argparse
 
 
 class Params:
-    size = (1, 1)
+    size = [0, 0]
     min_rotation = 0.0
     max_rotation = 360.0
-#probabilities of applying flip
     flip_horizontal = 0.5
     flip_vertical = 0.5
     width = 0
     height = 0
 
-    def __init__(self, size, min_rotation=0.0, max_rotation=360.0, flip_horizontal=0.5, flip_vertical=0.5, width=0, height=0):
+    def __init__(self, size=[0, 0], min_rotation=0.0, max_rotation=360.0, flip_horizontal=0.5, flip_vertical=0.5, width=0,
+                 height=0):
         self.size = size
         self.min_rotation = min_rotation
-        self.max_rotation = max_rotation
+        self.max_rotation = max(min_rotation, max_rotation)
         self.flip_horizontal = flip_horizontal
         self.flip_vertical = flip_vertical
         self.width = width
@@ -27,11 +27,10 @@ class Params:
 
 def process_image(input_path, output_folder, output_count, params):
     name = os.path.splitext(os.path.basename(input_path))[0]
-    extension = os.path.splitext(input_path)[1]
-
     date = datetime.now()
     output_folder += "/" + name + date.strftime("%m-%d-%Y-%H-%M-%S")
 
+    extension = os.path.splitext(input_path)[1]
     image_index = 0
     while image_index < output_count:
         output_path = output_folder + "/" + str(image_index) + extension
@@ -40,49 +39,113 @@ def process_image(input_path, output_folder, output_count, params):
 
         file = Image.open(input_path)
 
-        if file.size[0] < params.width or params.width == 0:
-            params.width = file.size[0]
-        if file.size[1] < params.height or params.height == 0:
-            params.width = file.size[1]
+        file = apply_transformations(file, params)
 
-        crop_x = random.randint(0, file.size[0] - params.width)
-        crop_y = random.randint(0, file.size[1] - params.height)
-
-        file = file.crop((crop_x, crop_y, crop_x + params.width, crop_y + params.height))
-
-        degrees = random.uniform(params.min_rotation, params.max_rotation)
-        file = file.rotate(degrees)
-
-        if random.random() < params.flip_horizontal:
-            file = file.transpose(Image.FLIP_LEFT_RIGHT)
-
-        if random.random() < params.flip_vertical:
-            file = file.transpose(Image.FLIP_TOP_BOTTOM)
-
-        file = file.resize(params.size)
         file.save(output_path, extension[1:])
         image_index += 1
 
 
-if len(sys.argv) < 2:
-    in_folder = os.getcwd()
-else:
-    in_folder = sys.argv[1]
-if len(sys.argv) == 3:
-    out_folder = sys.argv[2]
-else:
-    out_folder = in_folder
+def apply_transformations(file, params):
+    if file.size[0] < params.width or params.width == 0:
+        params.width = file.size[0]
+    if file.size[1] < params.height or params.height == 0:
+        params.height = file.size[1]
 
-paths = os.listdir(in_folder)
-parameters = Params((1000, 800), width=500, height=600)
-path_index = 0
-while path_index < len(paths):
-    ext = os.path.splitext(paths[path_index])[1]
-    if ext not in {".png", ".jpg", ".jpeg"}:
-        paths.remove(paths[path_index])
+    if params.size[0] == 0:
+        params.size[0] = file.size[0]
+    if params.size[1] == 0:
+        params.size[1] = file.size[1]
+
+    crop_x = random.randint(0, file.size[0] - params.width)
+    crop_y = random.randint(0, file.size[1] - params.height)
+
+    file = file.crop((crop_x, crop_y, crop_x + params.width, crop_y + params.height))
+
+    degrees = random.uniform(params.min_rotation, params.max_rotation)
+    file = file.rotate(degrees)
+
+    if random.random() < params.flip_horizontal:
+        file = file.transpose(Image.FLIP_LEFT_RIGHT)
+
+    if random.random() < params.flip_vertical:
+        file = file.transpose(Image.FLIP_TOP_BOTTOM)
+
+    return file.resize(tuple(params.size))
+
+
+def main(args):
+    if not args.input:
+        in_folder = os.getcwd()
     else:
-        path_index += 1
-for index, path in enumerate(paths):
-    process_image(in_folder + "/" + path, out_folder, 3, parameters)
-    print(str(index + 1) + "/" + str(len(paths)))
+        in_folder = args.input
+    if args.output:
+        out_folder = args.output
+    else:
+        out_folder = in_folder
 
+    paths = os.listdir(in_folder)
+    parameters = Params(size=args.size,
+                        min_rotation=args.minrot,
+                        max_rotation=args.maxrot,
+                        flip_horizontal=args.fliph,
+                        flip_vertical=args.flipv,
+                        width=args.width,
+                        height=args.height)
+
+    path_index = 0
+    while path_index < len(paths):
+        ext = os.path.splitext(paths[path_index])[1]
+        if ext not in {".png", ".jpg", ".jpeg"}:
+            paths.remove(paths[path_index])
+        else:
+            path_index += 1
+    for index, path in enumerate(paths):
+        process_image(in_folder + "/" + path, out_folder, int(args.count), parameters)
+        print(str(index + 1) + "/" + str(len(paths)))
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("count",
+                        help="Output images count for each input image",
+                        default=1,
+                        type=int)
+    parser.add_argument("--input",
+                        help="Input folder",
+                        default="")
+    parser.add_argument("--output",
+                        help="Output folder",
+                        default="")
+    parser.add_argument("--minrot",
+                        help="Min value of randomized degree for rotation",
+                        default=0,
+                        type=int)
+    parser.add_argument("--maxrot",
+                        help="Max value of randomized degree for rotation",
+                        default=360,
+                        type=int)
+    parser.add_argument("--fliph",
+                        help="Horizontal flip probability",
+                        default=0.5,
+                        type=float)
+    parser.add_argument("--flipv",
+                        help="Vertical flip probability",
+                        default=0.5,
+                        type=float)
+    parser.add_argument("--width",
+                        help="Width of crop window. Default (0) results in not cropping image",
+                        default=0,
+                        type=int)
+    parser.add_argument("--height",
+                        help="Height of crop window. Default (0) results in not cropping image",
+                        default=0,
+                        type=int)
+    parser.add_argument("--size",
+                        help="Output images size in format A,B. Default ((0,0)) results in not changing image size",
+                        default=[0, 0],
+                        nargs="+",
+                        type=int)
+    return parser.parse_args()
+
+
+main(parse_arguments())
