b92aeb3dd8f4f07beeac0dc9bfba31ce2fe7e151
KWoznicki <krzwozn@gmail.com>
2019-09-05 01:09:08 +0200

    *Code review changes

--- main.py
+++ main.py
@@ -26,101 +26,117 @@ class Params:
     sharpness: float = 1.0
 
 
-def process_image(input_path, output_folder, output_count, params):
+def process_image(input_path: str, output_folder: str, output_count: int, params: Params):
     name, extension = os.path.splitext(os.path.basename(input_path))
-    date = datetime.now()
+    date: str = datetime.now()
     output_folder = os.path.join(output_folder, "_".join([name, date.strftime("%m-%d-%Y-%H-%M-%S")]))
 
     for image_index in range(output_count):
-        output_path = os.path.join(output_folder, "".join([str(image_index), extension]))
+        output_path: str = os.path.join(output_folder, "".join([str(image_index), extension]))
         if not os.path.exists(output_folder):
             os.mkdir(output_folder)
 
-        file = Image.open(input_path)
+        image: Image = Image.open(input_path)
 
-        file = apply_transformations(file, params)
+        image = apply_transformations(image, params)
 
-        file.save(output_path, extension[1:])
+        image.save(output_path, extension[1:])
 
 
-def apply_transformations(file, params):
-    file_width, file_height = file.size
+def apply_transformations(image: Image, params: Params):
+    image_width, image_height = image.size
 
-    if file_width < params.crop_width or params.crop_width == 0:
-        params.crop_width = file_width
-    if file_height < params.crop_height or params.crop_height == 0:
-        params.crop_height = file_height
+    if image_width < params.crop_width or params.crop_width == 0:
+        params.crop_width = image_width
+    if image_height < params.crop_height or params.crop_height == 0:
+        params.crop_height = image_height
 
     if params.size is None:
-        params.size["width"] = file_width
-        params.size["height"] = file_height
+        params.size["width"] = image_width
+        params.size["height"] = image_height
 
-    crop_x = random.randint(0, file_width - params.crop_width)
-    crop_y = random.randint(0, file_height - params.crop_height)
+    crop_x: int = random.randint(0, image_width - params.crop_width)
+    crop_y: int = random.randint(0, image_height - params.crop_height)
 
-    file = file.crop((crop_x, crop_y, crop_x + params.crop_width, crop_y + params.crop_height))
+    image = image.crop((crop_x, crop_y, crop_x + params.crop_width, crop_y + params.crop_height))
 
-    degrees = random.uniform(params.min_rotation, params.max_rotation)
-    file = file.rotate(degrees)
+    degrees: float = random.uniform(params.min_rotation, params.max_rotation)
+    image = image.rotate(degrees)
 
     if random.random() < params.flip_horizontal:
-        file = file.transpose(Image.FLIP_LEFT_RIGHT)
+        image = image.transpose(Image.FLIP_LEFT_RIGHT)
 
     if random.random() < params.flip_vertical:
-        file = file.transpose(Image.FLIP_TOP_BOTTOM)
+        image = image.transpose(Image.FLIP_TOP_BOTTOM)
 
-    file = file.convert("RGB")
+    image = image.convert("RGB")
     if params.edges:
-        file = file.filter(ImageFilter.FIND_EDGES)
+        image = image.filter(ImageFilter.FIND_EDGES)
 
-    enhancer = ImageEnhance.Brightness(file)
-    file = enhancer.enhance(params.brightness)
+    enhancer = ImageEnhance.Brightness(image)
+    image = enhancer.enhance(params.brightness)
 
-    enhancer = ImageEnhance.Contrast(file)
-    file = enhancer.enhance(params.contrast)
+    enhancer = ImageEnhance.Contrast(image)
+    image = enhancer.enhance(params.contrast)
 
-    enhancer = ImageEnhance.Sharpness(file)
-    file = enhancer.enhance(params.sharpness)
+    enhancer = ImageEnhance.Sharpness(image)
+    image = enhancer.enhance(params.sharpness)
 
-    enhancer = ImageEnhance.Color(file)
-    file = enhancer.enhance(params.color)
+    enhancer = ImageEnhance.Color(image)
+    image = enhancer.enhance(params.color)
 
-    return file.resize(tuple(params.size))
+    return image.resize(tuple(params.size))
 
 
 def main(args):
-    if not args.input:
-        in_folder = os.getcwd()
-    else:
-        in_folder = args.input
-    if args.output:
-        out_folder = args.output
-    else:
-        out_folder = in_folder
 
-    paths = os.listdir(in_folder)
-    parameters = Params(size=args.size,
-                        min_rotation=args.minrot,
-                        max_rotation=args.maxrot,
-                        flip_horizontal=args.fliph,
-                        flip_vertical=args.flipv,
-                        crop_width=args.cwidth,
-                        crop_height=args.cheight,
-                        edges=args.edges,
-                        color=args.color,
-                        brightness=args.brightness,
-                        sharpness=args.sharpness,
-                        contrast=args.contrast)
+    invalid_path: bool = False
 
-    image_paths = []
+    if not os.path.exists(args.input):
+        print("Input folder does not exist")
+        invalid_path = True
+
+    if not os.path.exists(args.output):
+        print("Output folder does not exist")
+        invalid_path = True
+
+    if invalid_path:
+        return
+
+    paths: list = []
+
+    if args.recurse:
+        for root, directories, names in os.walk(args.input):
+            for name in names:
+                paths.append("/".join([root, name]))
+    else:
+        paths = os.listdir(args.input)
+
+    parameters: Params = Params(size=args.size,
+                                min_rotation=args.minrot,
+                                max_rotation=args.maxrot,
+                                flip_horizontal=args.fliph,
+                                flip_vertical=args.flipv,
+                                crop_width=args.cwidth,
+                                crop_height=args.cheight,
+                                edges=args.edges,
+                                color=args.color,
+                                brightness=args.brightness,
+                                sharpness=args.sharpness,
+                                contrast=args.contrast)
+
+    image_paths: list = []
 
     for path in paths:
-        ext = os.path.splitext(path)[1]
+        ext: str = os.path.splitext(path)[1]
         if ext in {".png", ".jpg", ".jpeg"}:
             image_paths.append(path)
 
+    if len(image_paths) == 0:
+        print("No images to process in input folder")
+
     for index, path in enumerate(image_paths):
-        process_image(os.path.join(in_folder, path), out_folder, int(args.count), parameters)
+        process_image(os.path.join(args.input, path), args.output, int(args.count), parameters)
         print("/".join([str(index + 1), str(len(image_paths))]))
 
 
@@ -128,14 +144,11 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("count",
                         help="Output images count for each input image",
-                        default=1,
                         type=int)
-    parser.add_argument("--input",
-                        help="Input folder",
-                        default="")
-    parser.add_argument("--output",
-                        help="Output folder",
-                        default="")
+    parser.add_argument("input",
+                        help="Input folder")
+    parser.add_argument("output",
+                        help="Output folder")
     parser.add_argument("--minrot",
                         help="Min value of randomized degree for rotation",
                         default=0,
@@ -185,6 +198,10 @@ def parse_arguments():
                         help="Adjust sharpness. 0.0 is blurred, 1.0 is original, 2.0 is sharpened",
                         default=1.0,
                         type=float)
+    parser.add_argument("-r",
+                        "--recurse",
+                        help="Process images in all child directories",
+                        action="store_true")
 
     return parser.parse_args()
 
